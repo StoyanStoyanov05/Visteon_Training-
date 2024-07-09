bab0ade21e1384bfa3f0cf72c0a17b6a392e12fd
StoyanStoyanov05 <StoyanStoyanov05@users.noreply.github.com>
2024-07-09 15:58:25 +0300

    addet suzanne example, default shaders and index buffer

---
+++ examples/gltf/04_suzanne/export/shaders/BasicShader.frag.glsl
@@ -0,0 +1,15 @@
+#version 300 es
+
+out highp vec4 FragColor;
+in highp vec2 vTexCoords;
+in highp vec3 vNormal;
+
+uniform highp vec3 color;
+
+void main()
+{
+
+	FragColor = vec4(color, 1.0);
+
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,24 @@
 
 using namespace std;
 
-
-void loadMaterial(tinygltf::Model& model,std::filesystem::path& gltfDirectory, unsigned int materialId)
+GLuint loadMaterial(tinygltf::Model& model,std::filesystem::path& gltfDirectory, unsigned int materialId)
 {
-
-    // const char* vertexShaderSource =
-    //     #include "vertexShader.glsl"
-    // ;
-
-
-    // const char* fragmentShaderSource =
-    //     #include "fragmentShader.glsl"
-    // ;
 
     std::filesystem::path vertexShaderPath;
     std::filesystem::path fragmentShadePath;
+     std::string vertexShaderSource;
+     std::string fragmentShaderSource;
 
+    const char* defaultVertexShaderSource =
+        #include "vertexShader.glsl"
+        ;
+
+    const char* defaultFragmentShaderSource =
+        #include "fragmentShader.glsl"
+        ;
+
+    if (materialId < model.materials.size())
+    {
     auto gltfMaterialExtras = model.materials[materialId].extras;
     if (gltfMaterialExtras.Has("shader"))
     {
@@ -36,16 +38,18 @@ void loadMaterial(tinygltf::Model& model,std::filesystem::path& gltfDirectory, u
             fragmentShadePath = gltfDirectory / gltfMaterialShaderFragment;
         }
     }
-
-
-    char* vertexShaderSource = nullptr;
-    char* fragmentShaderSource = nullptr;
+    }
+    else
+    {
+        vertexShaderSource = defaultVertexShaderSource;
+        fragmentShaderSource = defaultFragmentShaderSource;
+    }
+    
     std::ifstream vertexShaderFile(vertexShaderPath);
     if(vertexShaderFile.is_open())
     {
         std::string vertexShaderSourceString((std::istreambuf_iterator<char>(vertexShaderFile)), std::istreambuf_iterator<char>());
-        vertexShaderSource = new char[vertexShaderSourceString.length() + 1];
-        strcpy(vertexShaderSource, vertexShaderSourceString.c_str());
+        vertexShaderSource = vertexShaderSourceString;
         vertexShaderFile.close();
     }
 
@@ -53,16 +57,17 @@ void loadMaterial(tinygltf::Model& model,std::filesystem::path& gltfDirectory, u
     if(fragmentShaderFile.is_open())
     {
         std::string fragmentShaderSourceString((std::istreambuf_iterator<char>(fragmentShaderFile)), std::istreambuf_iterator<char>());
-        fragmentShaderSource = new char[fragmentShaderSourceString.length() + 1];
-        strcpy(fragmentShaderSource, fragmentShaderSourceString.c_str());
+        fragmentShaderSource = fragmentShaderSourceString;
         fragmentShaderFile.close();
     }
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader,1 ,&vertexShaderSource, nullptr);
+    const char* vertexShaderSourcePointer = vertexShaderSource.c_str();
+    glShaderSource(vertexShader,1 ,&vertexShaderSourcePointer, nullptr);
 
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    const char* fragmentShaderSourcePointer = fragmentShaderSource.c_str();
+    glShaderSource(fragmentShader, 1, &fragmentShaderSourcePointer, nullptr);
 
     glCompileShader(vertexShader);
     GLint status;
@@ -74,7 +79,7 @@ void loadMaterial(tinygltf::Model& model,std::filesystem::path& gltfDirectory, u
         std::vector<char> vertexShaderErrorMessage(infoLogLength);
         glGetShaderInfoLog(vertexShader, infoLogLength, nullptr, vertexShaderErrorMessage.data());
         std::cerr << "Failed to compile vertex shader: " << vertexShaderErrorMessage.data() << std::endl;
-        return;  
+        return -1;  
     }
 
     glCompileShader(fragmentShader);
@@ -86,7 +91,7 @@ void loadMaterial(tinygltf::Model& model,std::filesystem::path& gltfDirectory, u
         std::vector<char> fragmentShaderErrorMessage(infoLogLength);
         glGetShaderInfoLog(fragmentShader, infoLogLength, nullptr, fragmentShaderErrorMessage.data());
         std::cerr << "Failed to compile fragment shader: " << fragmentShaderErrorMessage.data() << std::endl;
-        return;  
+        return -1;
     }
 
     GLuint program = glCreateProgram();
@@ -102,10 +107,10 @@ void loadMaterial(tinygltf::Model& model,std::filesystem::path& gltfDirectory, u
         std::vector<char> programErrorMessage(infoLogLength);
         glGetProgramInfoLog(program, infoLogLength, nullptr, programErrorMessage.data());
         std::cerr << "Failed to link program: " << programErrorMessage.data() << std::endl;
-        return;  
+        return -1;
     }
-        
     glUseProgram(program);
+    return program;
 }
 int main(void)
 {
@@ -117,7 +122,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Monkey", NULL, NULL);
 
     if (!window)
     {
@@ -126,16 +131,15 @@ int main(void)
     }
 
         glfwMakeContextCurrent(window);
-        
 
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
     {
         glfwTerminate();
         return -1;
     }
-    // std::string gltfFilename = "../examples/gltf/01_triangle/export/triangle.gltf";
+    //std::string gltfFilename = "../examples/gltf/01_triangle/export/triangle.gltf";
 
-    std::string gltfFilename = "../examples/gltf/03_shaders/export/shaders.gltf";
+    std::string gltfFilename = "../examples/gltf/04_suzanne/export/suzanne.gltf";
     tinygltf::Model model;
     tinygltf::TinyGLTF loader;
     std:string err , warn;
@@ -148,110 +152,60 @@ int main(void)
         return 1;
     }
 
-    // GLfloat vertices[] = {
-    //     0.0f, 0.5f, 0.0f,
-    //     0.5f, -0.5f, 0.0f,
-    //     -0.5, -0.5f, 0.0f
-    
-    // }; 
-//     GLfloat vertexData[] = {
-//     -0.5f, -1.0f, 0.0f, // positions[0]​
-//     0.0f, 0.0f, 1.0f, // normals[0]​
-//     0.25f, 0.25f, // texture coordinates[0]​
-//     0.5f, -0.5f, 0.0f, // positions[1]​
-//     0.0f, 0.0f, 1.0f, // normals[1]​
-//     0.75f, 0.25f, // texture coordinates[1]​
-//     0.0f, 0.5f, 0.0f, // positions[2]​
-//     0.0f, 0.0f, 1.0f, // normals[2]​
-//     0.5f, 0.75f // texture coordinates[2]​
-    
-
-// };
-
-//     GLfloat vertexPositionData[] = {
-// -0.5f, -0.5f, 0.0f, // positions[0]​
-// 0.5f, -0.5f, 0.0f, // positions[1]​
-// 0.0f, 0.5f, 0.0f, // positions[2]​
-
-// };
-// GLfloat vertexNormalData[] = {
-// 0.0f, 0.0f, 1.0f, // normals[0]​
-// 0.0f, 0.0f, 1.0f, // normals[1]​
-// 0.0f, 0.0f, 1.0f, // normals[2]​
-
-// };
-// GLfloat vertexTexCoordData[] = {
-// 0.25f, 0.25f, // texture coordinates[0]​
-// 0.75f, 0.25f, // texture coordinates[1]​
-// 0.5f, 0.75f // texture coordinates[2]​
-
-// };
-
-// GLfloat vertexData[] = { 
-//     -0.5f, -0.5f, 0.0f, //
-//     0.5, -0.5, 0.0f, //
-//     0.0f, 0.5f, 0.0f, //
-//     0.0f, 0.0f, 1.0f, //
-//     0.0f, 0.0f, 1.0f, //
-//     0.0f, 0.0f, 1.0f, //
-//     0.25f, 0.25f, //
-//     0.75f, 0.25f, // 
-//     0.5, 0.75 //
-// };
-
     uint32_t gltfAccessorPositionIndex = model.meshes[0].primitives[0].attributes["POSITION"];
     uint32_t gltfAccessorNormalIndex = model.meshes[0].primitives[0].attributes["NORMAL"];
-    uint32_t gltfAccessorTexCoordIndex = model.meshes[0].primitives[0].attributes["TEXCOPED"];
+    uint32_t gltfAccessorTexCoordIndex = model.meshes[0].primitives[0].attributes["TEXCOORD_0"];
+    uint32_t gltfAccessorIndicesIndex = model.meshes[0].primitives[0].indices;
 
     uint32_t gltfBufferViewPositionIndex = model.accessors[gltfAccessorPositionIndex].bufferView;
     uint32_t gltfBufferViewNormalIndex = model.accessors[gltfAccessorNormalIndex].bufferView;
     uint32_t gltfBufferViewTexCoordIndex = model.accessors[gltfAccessorTexCoordIndex].bufferView;
+    uint32_t gltfBufferViewIdeicesIndex = model.accessors[gltfAccessorIndicesIndex].bufferView;
 
     uint32_t gltfBufferIndexPosition = model.bufferViews[gltfBufferViewPositionIndex].buffer;
     uint32_t gltfBufferIndexNormal = model.bufferViews[gltfBufferViewNormalIndex].buffer;
     uint32_t gltfBufferIndexTexCoord = model.bufferViews[gltfBufferViewTexCoordIndex].buffer;
+    uint32_t gltfBufferIndexIdeices = model.bufferViews[gltfBufferViewIdeicesIndex].buffer;
 
     unsigned char* gltfBufferDataPositon = model.buffers[gltfBufferIndexPosition].data.data();
     unsigned char* gltfBufferDataNormal = model.buffers[gltfBufferIndexNormal].data.data();
     unsigned char* gltfBufferDataTexCoord = model.buffers[gltfBufferIndexTexCoord].data.data();
+    unsigned char* gltfBufferDataIdeices = model.buffers[gltfBufferIndexIdeices].data.data();
 
     uint32_t gltfPositionByteOffset = model.bufferViews[gltfBufferViewPositionIndex].byteOffset;
     uint32_t gltfNormalByteOffset = model.bufferViews[gltfBufferViewNormalIndex].byteOffset;
     uint32_t gltfTexCoordByteOffset = model.bufferViews[gltfBufferViewTexCoordIndex].byteOffset;
+    uint32_t gltfIdeicesByteOffset = model.bufferViews[gltfBufferViewIdeicesIndex].byteOffset;
 
     uint32_t gltfPostionByteLength = model.bufferViews[gltfBufferViewPositionIndex].byteLength;
     uint32_t gltfNormalByteLength = model.bufferViews[gltfBufferViewNormalIndex].byteLength;
     uint32_t gltfTexCoordByteLength = model.bufferViews[gltfBufferViewTexCoordIndex].byteLength;
-
+    uint32_t gltfIndicesByteLength = model.bufferViews[gltfBufferViewIdeicesIndex].byteLength;
     
     // uint32_t  gltfBufferIndex = model.bufferViews[gltfPositionIndex].buffer;
     // unsigned char* gltfBufferData = model.buffers[gltfBufferIndex].data.data();
 
-
-    // float* vertices = (float*)gltfBufferData;
-
-    // for(int i = 0; i < 9;i++) 
-    // {
-    //     std::cout << vertices[i] << std::endl;
-    // }
-
+    GLint indices = gltfIndicesByteLength / sizeof(GLushort);
 
      GLuint vertexPositionBuffer = 0;
      glGenBuffers(1, &vertexPositionBuffer);
      glBindBuffer(GL_ARRAY_BUFFER, vertexPositionBuffer);
-     glBufferData(GL_ARRAY_BUFFER, gltfPostionByteLength, gltfBufferDataPositon, GL_STATIC_DRAW);
+     glBufferData(GL_ARRAY_BUFFER, gltfPostionByteLength, gltfBufferDataPositon + gltfPositionByteOffset, GL_STATIC_DRAW); 
 
+    GLuint IndexBuffer = 0;
+     glGenBuffers(1, &IndexBuffer);
+     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IndexBuffer);
+     glBufferData(GL_ELEMENT_ARRAY_BUFFER, gltfIndicesByteLength, gltfBufferDataIdeices + gltfIdeicesByteOffset, GL_STATIC_DRAW);
+    
      GLuint vertexNormalBuffer = 0;
      glGenBuffers(1, &vertexNormalBuffer);
      glBindBuffer(GL_ARRAY_BUFFER, vertexNormalBuffer);
-     glBufferData(GL_ARRAY_BUFFER, gltfNormalByteLength, gltfBufferDataNormal, GL_STATIC_DRAW);
+     glBufferData(GL_ARRAY_BUFFER, gltfNormalByteLength, gltfBufferDataNormal + gltfNormalByteOffset, GL_STATIC_DRAW);
 
      GLuint vertexTexCoordBuffer = 0;
      glGenBuffers(1, &vertexTexCoordBuffer);
      glBindBuffer(GL_ARRAY_BUFFER, vertexTexCoordBuffer);
-     glBufferData(GL_ARRAY_BUFFER, gltfTexCoordByteLength, gltfBufferDataTexCoord, GL_STATIC_DRAW);
-
-
+     glBufferData(GL_ARRAY_BUFFER, gltfTexCoordByteLength, gltfBufferDataTexCoord + gltfTexCoordByteOffset, GL_STATIC_DRAW);
     
     GLuint vertexArrayObject1 = 0;
     glGenVertexArrays(1,&vertexArrayObject1);
@@ -271,39 +225,33 @@ int main(void)
 
     std::filesystem::path gltfpath = gltfFilename;
     std::filesystem::path shaderpath = gltfpath.parent_path();
-    loadMaterial(model, shaderpath, 0);
 
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
-    // glBindVertexArray(0);
+    GLuint program = loadMaterial(model, shaderpath, 0);
 
 
-
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
 
     glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT); 
     
-    glBindVertexArray(vertexArrayObject1);
-    
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    // glBindVertexArray(0);
-    // glBindVertexArray(vertexArrayObject2);
+    glBindVertexArray(vertexArrayObject1);  
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IndexBuffer);
+   GLint uniformlocationColor = glGetUniformLocation(program, "color");
+   if(uniformlocationColor != -1)
+    {
 
-    // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUniform3f(uniformlocationColor, 1.0f, 0.0f, 0.0f);
+
+    }
+    glDrawElements(GL_TRIANGLES, indices, GL_UNSIGNED_SHORT, 0);
     glBindVertexArray(0);
-    // glBindVertexArray(vertexArrayObject2);
 
-    // glDrawArrays(GL_TRIANGLES, 0, 3);
     
 
-        // glBegin(GL_TRIANGLES);
-        // glVertex2f(-0.5f, -0.5f);
-        // glVertex2f( 0.0f, 0.5f);
-        // glVertex2f(0.5f, -0.5f);
-        // glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -313,7 +261,7 @@ int main(void)
     }
 
 
-    cout<<"check 7" << endl;
+    std::cout<<"check 7" << endl;
     glfwTerminate();
     return 0;
 }
